64caffad4e54c94dadb71e5591d34cc1601af6d5
it <it@uo000.zszatopkovych.cz>
2019-12-05 09:22:28 +0100

    Frostbite class update

--- src/minecraft/me/adam077x/frostbite/Frostbite.java
+++ src/minecraft/me/adam077x/frostbite/Frostbite.java
@@ -1,13 +1,16 @@
 package me.adam077x.frostbite;
 
+import me.adam077x.frostbite.command.CommandManager;
 import me.adam077x.frostbite.module.ModuleManager;
 
 public class Frostbite {
 	public static final boolean prerelease = true;
 	public static ModuleManager moduleManager = new ModuleManager();
+	public static CommandManager commandManager = new CommandManager();
 	
 	public void startClient() {
 		moduleManager.registerModules();
+		commandManager.registerCommands();
 	}
 	
 	public static String getClientName() {
